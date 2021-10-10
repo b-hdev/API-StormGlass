16ac573a7e5e94c90df0424a01574a50a921fd93
HellpMe <bruno.bh969@gmail.com>
2021-10-09 21:18:53 -0300

    beaches controllers

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { Application } from 'express';
 import { Server } from '@overnightjs/core';
 import * as database from '@src/database/mongoose';
 import { ForecastController } from './controllers/forecast';
+import { BeachesController } from './controllers/beaches';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -23,7 +24,8 @@ export class SetupServer extends Server {
 
   private setupControllers(): void {
     const forecastController = new ForecastController();
-    this.addControllers([forecastController]);
+    const beachesController = new BeachesController();
+    this.addControllers([forecastController, beachesController]);
   }
 
   public getApp(): Application {
